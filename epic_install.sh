da564ddeff85624940c9f5f1e28778752cc84f54
Aksel <axel.akonde@gmail.com>
2022-03-18 18:54:13 +0100

    epic_install utilisable

--- epic_install.sh
+++ epic_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+current_user=$(whoami)
+echo "${$current_user}"
+
 echo "Epic Events Installation."
 echo "The environment and requirements will now be installed..."
 python3.9 -m virtualenv venv
@@ -8,23 +11,25 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "Installation of the Epic Events Database..."
+echo "This requires root actions. Please enter your password when asked"
 sudo su postgres <<EOF
 service postgresql restart
 psql -c 'DROP DATABASE epicdb;'
 psql -c 'CREATE DATABASE epicdb;'
+psql -c "DROP USER epicuser;"
 psql -c "CREATE USER epicuser WITH PASSWORD 'djangepic';"
 psql -c 'GRANT ALL PRIVILEGES on DATABASE epicdb to epicuser;'
 EOF
 
-echo "Log: on reactive le venv"
+sudo su "${$current_user}"
+
 source venv/bin/activate
-echo "Log: on change de dossier epic_events"
 cd epic_events/
-echo "Log: makemigrations"
+echo "creating database tables models"
 python manage.py makemigrations
-echo "Log: migrate"
+echo "creating tables in database"
 python manage.py migrate
-echo "Log: load data fixture"
+echo "inserting default data"
 python manage.py loaddata core/fixtures/departments_fixtures.json
 
 echo "Epic Events Installation successfully completed !"
