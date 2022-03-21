7adb2ae703652f37ee2335a448047468cd842101
ax <axel.akonde@cinego.net>
2022-03-21 14:35:22 +0100

    pb authentification resolu et menage dans settings.py pour SIMPLEJWT

--- epic_events/permissions/permissions.py
+++ epic_events/permissions/permissions.py
@@ -4,10 +4,10 @@ from constants import MANAGER, SALES, SUPPORT
 from core.models import Employee
 
 
-class IsManager(IsAuthenticated):
+class IsManager(IsAuthenticated): # mais en fait il faut faire des permissions par Objet plutot ...
     def has_permission(self, request, view):
-        employee = Employee.objects.get(id=request.user)  # request.user.id = None !!!
-        return bool(MANAGER in employee.groups)
+        employee = Employee.objects.get(id=request.user.id)
+        return employee.groups.filter(id=MANAGER).exists()
 
     def has_object_permission(self, request, view, obj):
         pass
