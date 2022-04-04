78f23f559fe20b6c02114bb6a9d9e303a6558709
ax <axel.akonde@cinego.net>
2022-04-04 12:46:38 +0200

    modif script bash epic_install, ajout du host et du port de DB avec une valeur par défaut

--- epic_install.sh
+++ epic_install.sh
@@ -20,6 +20,25 @@ printf "\nPlease provide a Django username for the PostgreSQL Database: \n"
 read DB_USER
 printf "\nPlease provide a password for the Django user: \n"
 read DB_PASSWORD
+
+printf "\nThe Defaut postgreSQL database host is 'localhost'. Do you want to change this settings?\n"
+select yn in "No" "Yes"; do
+  case $yn in
+    No) DB_HOST='localhost'; break;;
+    Yes) read -p "Please provide the new database host:" DB_HOST; break;;
+  esac
+done
+printf "DB_HOST will be: '$DB_HOST' ";
+
+printf "\nThe Defaut postgreSQL database port is '5432'. Do you want to change this settings?\n"
+select yn in "No" "Yes"; do
+  case $yn in
+    No) DB_PORT='5432'; break;;
+    Yes) read -p "Please provide the new database Port: " DB_PORT; break;;
+  esac
+done
+printf "DB_PORT will be: '$DB_PORT' ";
+
 printf "\nPlease provide a name for the app logs file: \n"
 read PATH_TO_LOGS
 
@@ -27,6 +46,8 @@ printf "SECRET_KEY = '$SECRET_KEY'\n
 DB_NAME = '$DB_NAME'\n
 DB_USER = '$DB_USER'\n
 DB_PASSWORD = '$DB_PASSWORD'\n
+DB_HOST = '$DB_HOST'\n
+DB_PORT = '$DB_PORT'\n
 PATH_TO_LOGS = '$PATH_TO_LOGS'\n" > epic_events/.env
 
 touch epic_events/$PATH_TO_LOGS
