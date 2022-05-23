59db448368e75810775a146cae77ff881ea45f65
Aksel <axel.akonde@gmail.com>
2022-05-23 10:01:14 +0200

    essais search field for string to solve exact/contains pb

--- epic_events/api/views.py
+++ epic_events/api/views.py
@@ -83,7 +83,8 @@ class ContractModelViewSet(ModelViewSet):
     permission_classes = (ContractPermissions,)
     serializer_class = ContractSerializer
     filterset_class = ContractFilter
-    # pb car ne prend plus le filtre général , seulement le DatedItemFilter, les autres filtres sont désactivés
+    # search_fields = ['client__last_name', 'client__email']
+    # pb avec le filter : only exxact, voir avec search fields si possible de resoudre
     filterset_fields = ['id', 'client__last_name', 'client__email', 'amount_in_cts',
                         'date_created', 'date_updated']
 
